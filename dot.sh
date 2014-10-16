8aae9a0c6471a397bad4d65c3b0ec776a71c2264
Colin Ashburn <cashburn@purdue.edu>
2014-10-16 18:05:37 -0400

    2014-10-16

--- dot.sh
+++ dot.sh
@@ -4,6 +4,8 @@ function github_clone(){
   git clone git://github.com/$1 $2
 }
 
+git config --global user.name "Colin Ashburn"
+git config --global user.email cashburn@purdue.edu
 github_clone "robbyrussell/oh-my-zsh.git" "$HOME/.oh-my-zsh"
 github_clone "zsh-users/zsh-syntax-highlighting.git" "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 github_clone "zsh-users/zsh-history-substring-search" "$HOME/.oh-my-zsh/custom/plugins/zsh-history-substring-search"
