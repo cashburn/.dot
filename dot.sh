1b96a288bc0f959d07ba6e2b2d22f24f6f215673
Colin Ashburn <cashburn@purdue.edu>
2014-10-16 00:02:00 -0400

    First Upload

---
+++ dot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function github_clone(){
+  git clone git://github.com/$1 $2
+}
+
+github_clone "robbyrussell/oh-my-zsh.git" "$HOME/.oh-my-zsh"
+github_clone "zsh-users/zsh-syntax-highlighting.git" "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+
+ln -s "$HOME/.dot/zshrc" "$HOME/.zshrc"
+ln -s "$HOME/.dot/cashburn.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/cashburn.zsh-theme"
+chsh -S /bin/zsh
+
