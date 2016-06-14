f8bb8e86cc6dc2bd81bb5380d530da9a4052dfd2
Colin Ashburn <cashburn@purdue.edu>
2016-06-14 02:26:43 +0000

    2016-06-14

--- dot.sh
+++ dot.sh
@@ -7,6 +7,8 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" #Makes shell turn green when you type a valid command.
 git clone git://github.com/zsh-users/zsh-history-substring-search "$HOME/.oh-my-zsh/custom/plugins/zsh-history-substring-search" #Type any portion of a past command then press up/down arrows to scroll through use.
 
+source "vim.sh"
+
 #Add any other zsh plugins you want.
 
 ln -s "$HOME/.dot/zshrc" "$HOME/.zshrc" #A customized zsh config file.
