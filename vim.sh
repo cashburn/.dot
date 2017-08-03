7fc38401f5e8eb21713fc88ecbc07ba3a844e149
Colin Ashburn <cashburn@purdue.edu>
2017-08-03 17:15:53 -0500

    Improved OneDark syntax highlighting

--- vim.sh
+++ vim.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/ervandew/supertab ~/.vim/bundle/supertab
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
 mkdir ~/.vim/colors
+curl -LSso ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim
 curl -LSso ~/.vim/colors/hybrid.vim https://raw.githubusercontent.com/flazz/vim-colorschemes/master/colors/hybrid.vim
-curl -LSso ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/master/colors/onedark.vim
+cp onedark.vim ~/.vim/colors/
+unzip syntax.zip -d ~/.vim/
--- vimrc
+++ vimrc
@@ -27,11 +27,11 @@ set statusline+=%#warningmsg#
 set statusline+=%{SyntasticStatuslineFlag()}
 set statusline+=%*
 
-let g:syntastic_always_populate_loc_list = 1
-let g:syntastic_auto_loc_list = 1
-let g:syntastic_check_on_open = 1
-let g:syntastic_check_on_wq = 0
-let g:syntastic_cpp_check_header = 1
+"let g:syntastic_always_populate_loc_list = 1
+"let g:syntastic_auto_loc_list = 1
+"let g:syntastic_check_on_open = 1
+"let g:syntastic_check_on_wq = 0
+"let g:syntastic_cpp_check_header = 1
 
 "keyboard mappings
 map <F1> :w<CR>
