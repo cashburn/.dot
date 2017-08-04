70ac2389f5fc870a2557453d6101df8c318a51fa
Colin Ashburn <cashburn@purdue.edu>
2017-08-03 19:16:17 -0500

    2017-08-03

--- vim.sh
+++ vim.sh
@@ -2,7 +2,6 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/ervandew/supertab ~/.vim/bundle/supertab
-git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
 mkdir ~/.vim/colors
 curl -LSso ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/master/autoload/onedark.vim
 curl -LSso ~/.vim/colors/hybrid.vim https://raw.githubusercontent.com/flazz/vim-colorschemes/master/colors/hybrid.vim
